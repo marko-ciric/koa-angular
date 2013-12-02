e76b7c9093b573be70344dd27a22483aefe7356a
Marko Ciric <ciric.marko@gmail.com>
2013-12-02 09:51:46 +0100

    Initial commit

---
+++ public/javascripts/MainController.js
@@ -0,0 +1,23 @@
+var module = angular.module('instagram-feed', ['ngResource']);
+
+module.factory('Users', ['$resource', function ($resource) {
+    return $resource('/api/users', {id: '@id'});
+}]);
+
+module.controller('MainController', ['$scope', 'Users', function ($scope, Users) {
+
+    $scope.users = [
+        {id: 1, name: 'Marko', photos: [
+            {name: 'A'},
+            {name: 'B'}
+        ]},
+        {id: 2, name: 'Nikola', photos: [
+            {name: 'A'},
+            {name: 'B'}
+        ]},
+        {id: 3, name: 'Ivan', photos: [
+            {name: 'A'},
+            {name: 'B'}
+        ]}
+    ];
+}]);
