1a934f3bbcb072b359b703bd4f82c3ef492369eb
Marko Ciric <ciric.marko@gmail.com>
2014-01-12 18:44:28 +0100

    Replaced expressjs with koajs.

--- app.js
+++ app.js
@@ -2,27 +2,36 @@
 /**
  * Module dependencies.
  */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
+var koa = require('koa');
+var route = require('koa-route');
 var path = require('path');
 
-var app = express();
+var routes = require('./routes');
+var user = require('./routes/user');
+
+var app = koa();
+
+var port = 3000;
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.name = 'koa-angular-demo';
+app.env = 'development';
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+
+
+// Replace with koa middleware .
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser('your secret here'));
+//app.use(express.session());
+//app.use(app.router);
+
 app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -34,6 +43,4 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(port);
