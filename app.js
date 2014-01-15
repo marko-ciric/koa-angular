07075d93d20c0cbe53b5c86522e8e76efdf472a3
Marko Ciric <ciric.marko@gmail.com>
2014-01-15 23:06:19 +0100

    Added common koa middleware to dependency.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
+var responsetime = require('koa-response-time');
 var http = require('http');
 
+var session = require('koa-session');
 var route = require('koa-route');
 var views = require('co-views');
 var parse = require('co-body');
+
 var koa = require('koa');
 
 var port = 3000;
 
 var app = koa();
+app.use(responsetime);
 app.name = 'koa-angular-demo';
 app.env = 'development';
+
+app.use(rewrite('/views/', '/'));
 
 app.use(function *() {
     this.body = "Hello world!";
