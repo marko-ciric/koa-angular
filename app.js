c4f604f08bcfc6bb2ec7bc4b35f50dccedc56812
Marko Ciric <ciric.marko@gmail.com>
2014-01-15 23:48:37 +0100

    Added middleware responseTime and logging requests.

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 
 var responsetime = require('koa-response-time');
-var http = require('http');
-
-var session = require('koa-session');
-var route = require('koa-route');
-var views = require('co-views');
-var parse = require('co-body');
-
+var compose = require('koa-compose');
 var koa = require('koa');
+var logger = require('koa-logger');
 
-var port = 3000;
+var app = module.exports = koa();
 
-var app = koa();
-app.use(responsetime);
 app.name = 'koa-angular-demo';
 app.env = 'development';
 
-app.use(rewrite('/views/', '/'));
+function *responseTime(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + 'ms');
+}
 
-app.use(function *() {
-    this.body = "Hello world!";
+// logger
+
+function* logger(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    if ('test' != process.env.NODE_ENV) {
+        console.log('%s %s - %s', this.method, this.url, ms);
+    }
+}
+
+app.use(compose([responseTime, logger]));
+
+app.use(function *(){
+    this.body = 'Hello World';
 });
 
-app.listen(port);
+if ()app.listen(3000);
 
