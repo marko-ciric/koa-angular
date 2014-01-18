81a19e642ee418eb9fd0017f26093aba8389bc2f
Marko Ciric <ciric.marko@gmail.com>
2014-01-19 00:25:15 +0100

    compose-style of app functions declaration.

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-
 var responsetime = require('koa-response-time');
 var common = require('koa-common');
 var route = require('koa-route');
+var parse = require('co-body');
 var compose = require('koa-compose');
 var koa = require('koa');
+
 
 var app = module.exports = koa();
 
 app.name = 'koa-angular-demo';
 app.env = 'development';
 
-function *responseTime(next){
+function* responseTime(next) {
     var start = new Date;
     yield next;
     var ms = new Date - start;
     this.set('X-Response-Time', ms + 'ms');
 }
 
-// logger
-
-function* logger(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    if ('test' != process.env.NODE_ENV) {
-        console.log('%s %s - %s', this.method, this.url, ms);
-    }
-}
-
-app.use(compose([responseTime, logger]));
-
-app.use(function *(){
+app.use(compose([responseTime, common.logger(), common.favicon(), function* (next) {
+    console.log('Requested: ', this.request.url);
     this.body = 'Hello World';
-});
+    yield next;
+}]));
 
 app.listen(3000);
-
