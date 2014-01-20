33f2a9cd9de32b78e90a739e64761fec1e0470a3
Marko Ciric <ciric.marko@gmail.com>
2014-01-20 09:43:32 +0100

    More dependencies.

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 var parse = require('co-body');
-
 var route = require('koa-route');
-var koa = require('koa');
-
-var common = require('./lib/common')
-
 var compose = require('koa-compose');
+var common = require('./lib/common')
+var koa = require('koa');
 
 var app = module.exports = koa();
 
 app.name = 'koa-angular-demo';
-
 app.env = 'development';
+app.keys = ['koa-angular', 'key'];
 
-app.use(compose([common.logger(), common.responseTime(),
+app.use(compose([common.logger(), common.responseTime(), common.favicon(), common.compress(),
     route.get('/', function* () {
-        "use strict";
         this.body = 'Hello world!';
     }),
-    route.get('/users', function* () {
-        "use strict";
-        this.body = 'Hello users!';
+    route.get('/user', function* () {
+        var object = common.printObj(this.path);
+        this.body = object + ': Hello users!';
+    }),
+    route.get('/user/:action/:id', function* (action, id) {
+        this.body = action + ' user ' + id;
+    }),
+    route.get('/post', function* () {
+        this.body = 'Hello post.';
+    }),
+    route.get('/post/:id', function* (id) {
+        this.body = id;
     })
-
 ]));
 
 app.listen(3000);
